8a8341ede9cde09b86b91961e247042bab76150b
chenyan23 <100217874+chenyan23@users.noreply.github.com>
2022-03-07 19:38:14 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 targetImage.addEventListener("click", function() {
   targetImage.classList.remove("small");
 }); 
-});
-document.addEventListener("DOMContentLoaded", function(event) {
-   let targetImage1 = document.querySelector("#big-image");
+    let targetImage1 = document.querySelector("#big-image");
 targetImage1.addEventListener("click", function() {
   targetImage1.classList.remove("small");
-}); 
 });
+ });  
+
